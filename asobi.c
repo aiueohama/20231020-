9eb1b0fab2c1ff3a1a47e267d540cddfcee4e814
aiueohama <148538013+aiueohama@users.noreply.github.com>
2023-10-27 19:21:06 +0900

    Add files via upload

---
+++ asobi.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	int a=0;
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf_s("%d", &a);
+	if (a % 2 == 0)printf("’l‚Í‹ô”‚Å‚·");
+	else if (a % 2 != 0)printf("’l‚ÍŠï”‚Å‚·");
+	return 0;
+}
