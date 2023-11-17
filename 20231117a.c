b6463fa14025e24bd2dfe192229449956a5809e2
aiueohama <148538013+aiueohama@users.noreply.github.com>
2023-11-17 19:38:10 +0900

    Add files via upload

---
+++ 20231117a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main(void)
+{
+	char b;
+	printf("“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F");
+	scanf_s("%c",&b);
+	if (isupper(b))printf("‘å•¶š‚ªŠÜ‚Ü‚ê‚Ä‚¢‚Ü‚·\n");
+return 0;
+}
